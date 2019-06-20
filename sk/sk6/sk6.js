2f250583348822adc254bd40504046dcdcbf313e
Gil Fuser <gilfuser@gmail.com>
2019-06-20 00:50:18 -0300

    new layout

---
+++ sk/sk6/sk6.js
@@ -0,0 +1,64 @@
+const sk6 = sk => {
+  let wiggle = { wX: 0, wY: 0, wW: 0, wH: 0, wA: 0 }
+  let col = { verm: 0, verde: 0, azul: 0 }
+  let mouseXtemp = 0
+  let mouseYtemp = 0
+  let xoff = 0.0
+  let yoff = 0.02
+  let posX
+  let posY
+  let largura = 100
+  let altura = 100
+  sk.setup = () => {
+    sk.createCanvas(sk.windowWidth, sk.windowHeight)
+    posX = sk.windowWidth * 0.5 - largura * 0.5
+    posY = sk.windowHeight * 0.5 - altura * 0.5
+    sk.frameRate(30)
+  }
+  sk.draw = () => {
+    sk.background(col.verm, 0, col.azul, wiggle.wA)
+    sk.noStroke()
+    mouseXtemp = sk.map(sk.abs(mouseXtemp - sk.mouseX), 0, 400, 5, 150)
+    mouseYtemp = sk.map(sk.abs(mouseYtemp - sk.mouseY), 0, 300, 5, 150)
+    wiggle.wX = sk.random(-1 * mouseXtemp, mouseXtemp)
+    wiggle.wY = sk.random(-1 * mouseYtemp, mouseYtemp)
+    wiggle.wW = sk.random(-1 * mouseXtemp, mouseXtemp)
+    wiggle.wH = sk.random(-1 * mouseYtemp, mouseYtemp)
+    wiggle.wA = sk.random(0, 3)
+    sk.fill(255, 32 + wiggle.wA)
+    xoff = xoff + 0.01
+    xoff = yoff + 0.02
+    let n = sk.noise(xoff) * 10
+    let m = sk.noise(yoff) * 10
+    largura = wiggle.wW * n - (mouseYtemp + mouseXtemp) + 100
+    altura = wiggle.wH * m - (mouseYtemp + mouseXtemp) + 100
+    sk.rect(posX, posY, largura + mouseXtemp, altura + mouseYtemp)
+    if (
+      sk.mouseX > posX &&
+      sk.mouseX < posX + largura &&
+      sk.mouseY > posY &&
+      sk.mouseY < posY + altura
+    ) {
+      if (mouseIsPressed) {
+        largura = largura + (mouseYtemp + mouseXtemp)
+        altura = altura + (mouseYtemp + mouseXtemp)
+        posX = sk.mouseX - largura * 0.5
+        posY = sk.mouseY - altura * 0.5
+      }
+    }
+    posX = posX + wiggle.wX
+    posY = posY + wiggle.wY
+    mouseXtemp = sk.mouseX
+    mouseYtemp = sk.mouseY
+  }
+  sk.windowResized = () => {
+    sk.resizeCanvas(sk.windowWidth, sk.windowHeight)
+    sk.background(col.verm, 0, col.azul, wiggle.wA)
+  }
+  sk.mouseReleased = () => {
+    posX = sk.random(largura, sk.windowWidth - largura)
+    posY = sk.random(altura, sk.windowHeight - altura)
+    col.verm = sk.random(0, 256)
+    col.azul = sk.random(0, 129)
+  }
+}
---
+++ sk/sk9/sk9.js
@@ -0,0 +1,109 @@
+const sk9 = sk => {
+  let wiggle = { wX: 0, wY: 0, wW: 0, wH: 0, wA: 0 }
+  let col = { verm: 0, verde: 0, azul: 0 }
+  let mouseXtemp = 0
+  let mouseYtemp = 0
+  let xoff = 0.0
+  let yoff = 0.02
+  let posXloop
+  let posYloop
+  let largura = 100
+  let altura = 100
+  // let nums = [20, 16, 84, 92, 139]
+  // let num = 184
+  // let palavras = ['amor', 'saúde', 'esperanca', 'música', 'supercollider']
+  let index = 0
+  let uoff = 0.0
+  let voff = 0.0
+  let zoff = 0.0
+  let noiseScale = 0.02
+  let heads = []
+  sk.setup = () => {
+    sk.createCanvas(sk.windowWidth, sk.windowHeight)
+    for (i = 0; i < 100; i++) {
+      heads[i] = new Heads()
+    }
+  }
+  sk.draw = () => {
+    mouseXtemp = mouseXtemp - sk.mouseX
+    mouseYtemp = mouseYtemp - sk.mouseY
+    zoff = zoff + 0.1
+    let t = sk.noise(zoff) * 5.5
+    wiggle.wA = t + wiggle.wA * 0.1
+    sk.background(col.verm, 0, col.azul, wiggle.wA)
+    sk.fill(255, 32 + wiggle.wA)
+    // fill(255)
+    // textSize(25)
+    // text(palavras[index], 20, 200)
+    for (let i = 0; i < heads.length; i++) {
+      heads[i].jitter()
+      heads[i].window()
+    }
+    mouseXtemp = sk.mouseX
+    mouseYtemp = sk.mouseY
+  }
+  sk.windowResized = () => {
+    sk.resizeCanvas(sk.windowWidth, sk.windowHeight)
+    sk.background(col.verm, 0, col.azul, wiggle.wA)
+  }
+  sk.mousePressed = () => {
+    for (let i = 0; i < 11; i++) {
+      heads[i].x = sk.random(0.1, sk.windowWidth)
+      heads[i].xOfst = sk.random(0.1, sk.windowWidth)
+      heads[i].y = sk.random(0.1, sk.windowHeight)
+      heads[i].yOfst = sk.random(0, sk.windowHeight)
+      heads[i].w = sk.random(0.1, sk.windowHeight * 0.2)
+      heads[i].h = sk.random(0.1, sk.windowHeight * 0.2)
+      heads[i].wB = sk.random(0.1, sk.windowHeight * 0.2)
+      heads[i].hB = sk.random(0.11, sk.windowHeight * 0.2)
+      heads[i].walk = sk.random(0.1, 10.1)
+    }
+    col.verm = sk.random(0, 250)
+    col.azul = sk.random(0, 129)
+  }
+
+  Heads = function () {
+    this.x = sk.random(0.1, sk.windowWidth)
+    this.xOfst = sk.random(0.1, sk.windowWidth)
+    this.y = sk.random(0.1, sk.windowHeight)
+    this.yOfst = sk.random(0, sk.windowHeight)
+    this.w = sk.random(sk.windowHeight * 0.01, sk.windowHeight * 0.2)
+    this.h = sk.random(sk.windowHeight * 0.01, sk.windowHeight * 0.2)
+    this.wB = sk.random(sk.windowHeight * 0.01, sk.windowHeight * 0.2)
+    this.hB = sk.random(sk.windowHeight * 0.01, sk.windowHeight * 0.2)
+    this.walk = sk.random(0.1, 10.1)
+    this.window = function () {
+      sk.stroke(255)
+      sk.fill(255, 255, 255, 20)
+      sk.rect(this.x, this.y, this.w, this.h)
+    }
+    this.jitter = function () {
+      xoff = xoff + 0.51
+      yoff = yoff + 0.51
+      uoff = xoff + 0.51
+      voff = yoff + 0.51
+
+      let m = sk.map(sk.noise(yoff), 0, 1, -0.5, 0.5) * this.walk
+      let n = sk.map(sk.noise(yoff), 0, 1, -0.5, 0.5) * this.walk
+      let u = sk.map(sk.noise(uoff), 0, 1, -0.5, 0.5) * this.walk
+      let v = sk.map(sk.noise(voff), 0, 1, -0.5, 0.5) * this.walk
+      this.x = -this.x
+      this.w = this.w + u
+      this.w = -this.w
+      this.h = this.h + v
+      this.h = -this.h
+      this.x = this.xOfst - this.w * 0.5 + n
+      this.x = this.x + n
+      this.y = this.yOfst - this.h * 0.5 + m
+      this.y = this.y + m
+    }
+    return this
+  }
+  sk.mouseReleased = () => {
+    posX = sk.random(largura, sk.windowWidth - largura)
+    posY = sk.random(altura, sk.windowHeight - altura)
+    col.verm = sk.random(0, 256)
+    col.azul = sk.random(0, 129)
+    sk.background(col.verm, 0, col.azul)
+  }
+}
